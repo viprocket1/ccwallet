abfcc2faecf5cbab5ed22dd0e59f5e6f2189f24b
dam num <47239352+viprocket1@users.noreply.github.com>
2026-01-12 17:37:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,12 @@
+// src/main.jsx (or src/index.js)
+import { Buffer } from 'buffer';
+import process from 'process';
+
+// Polyfill immediately before any other imports
+window.global = window;
+window.process = process;
+window.Buffer = Buffer;
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
