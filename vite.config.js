cce6416e74f9a91a3d46900434dfd8060d581f0d
dam num <47239352+viprocket1@users.noreply.github.com>
2026-01-12 17:27:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ADD THIS SECTION
+  define: {
+    'process.env': {},
+  },
 })
