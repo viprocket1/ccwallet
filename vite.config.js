e5ac6b2909ef3d803bec721b687c62ecaefa9290
dam num <47239352+viprocket1@users.noreply.github.com>
2026-01-12 17:39:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills' // Optional: simpler if you install this plugin
 
-// https://vitejs.dev/config/
+// OR use this manual config:
 export default defineConfig({
   plugins: [react()],
-  // ADD THIS SECTION
   define: {
     'process.env': {},
+    'global': 'window',
   },
 })
